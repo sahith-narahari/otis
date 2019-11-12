8356ddec1a1305d5e221875aa9bbc752d9a77f9b
saren <sahithnarahari@gmail.com>
2019-11-12 18:35:57 +0530

    Added alert

--- alert/alert.go
+++ alert/alert.go
@@ -2,11 +2,44 @@ package alert
 
 import (
 	"fmt"
+	"log"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/sahith-narahari/otis/config"
 )
 
 func SendAlert() error {
-	fmt.Println("this is token",config.NewApp.Token)
-	fmt.Println("this is port",config.NewApp.Port)
+	bot, err := tgbotapi.NewBotAPI(config.NewApp.Token)
+	if err != nil {
+		fmt.Println("Error starting bot", err.Error())
+		return err
+	}
+	bot.Debug = true
+
+	update := tgbotapi.NewUpdate(0)
+	update.Timeout = 60
+
+	chanUpdates, err := bot.GetUpdatesChan(update)
+	if err != nil{
+		fmt.Printf("unable to get updates")
+	}
+
+	for updates := range chanUpdates {
+		if updates.Message == nil {
+			continue
+		}
+
+		if updates.Message.IsCommand() {
+			msg := tgbotapi.NewMessage(updates.Message.Chat.ID, "hello")
+			switch updates.Message.Command() {
+			case "start":
+				GetStatus()
+			default:
+				msg.Text = "right command please"
+			}
+			if _, err :=bot.Send(msg);err != nil {
+				log.Panic( err)
+			}
+		}
+	}
 	return nil
 }
---
+++ alert/status.go
@@ -0,0 +1,5 @@
+package alert
+
+func GetStatus() string{
+	return "hello"
+}
